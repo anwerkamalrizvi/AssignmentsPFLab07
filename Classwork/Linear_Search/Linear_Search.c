0ff73c00985b4abb0be96608ee4059f27e996a06
anwerkamalrizvi <sanwerkamal@gmail.com>
2024-10-08 21:01:21 +0500

    Add files via upload

---
+++ Classwork/Linear_Search/Linear_Search.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(){
+	int search;
+	
+	int n;
+	printf("Enter length of the array: ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("\nEnter %d elements:\n", n);
+	int i;
+	for(i=0;i<n;i++){
+        	printf("Element no.%d: ",i+1);
+        	scanf("%d",&arr[i]);
+	}//end for 1
+	printf("Search: ");
+	scanf("%d",&search);
+	
+	printf("Your entered series is ");
+	for(i=0;i<n;i++){
+		printf("%d, ",arr[i]);
+	}//end for
+	
+	for(i=0;i<n;i++){
+
+		if (search==arr[i]){ 
+			
+			printf("\n%d Found at [%d]",search,  i);
+			break;
+		}
+			else {	
+			continue;
+		}
+	}//end for
+	
+	(search!=arr[i]) ?  printf("\n%d is not found",search) : printf(""); 
+	return 0;
+}
